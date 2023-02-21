4df0f353948d12035efbc2c03312601eb3d28655
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-21 15:09:56 +0530

    Addition of variables

---
+++ Addition of  variable.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+  int a = 23 ; //Assign the value 23 to a
+  int b = 20 ; //Assign the value 20 to b
+  printf("%d", a + b ); //printf the desired output
+  return 0;
