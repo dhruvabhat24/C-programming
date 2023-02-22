3aaa4bfbce052b06c1e93a327533c55fb722c888
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-22 14:30:56 +0530

    Area of a circle

---
+++ Area of a circle.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  double pi = 3.14 ;
+  double radius = 8.9;      //radius has to be declared as a 'double'
+  double area = pi * radius * radius;
+  printf("The Area of the given Circle is %lf", area ); 
+  return 0;
+}
