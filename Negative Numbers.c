65dda3189db7d6a8b454be2d24d4a46896ea7b65
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-23 14:59:47 +0530

    Negative Numbers

---
+++ Negative Numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int a = -50 ;
+  int b = 40  ;
+  int sum = a + b ;
+  int product = a * b ;
+  int quotient = a / b;
+  printf("%d \n", sum); 
+  printf("%d \n", product ); 
+  printf("%d \n", quotient); 
+  return 0;
