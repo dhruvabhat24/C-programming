b4dd4eb7d1751e41d0a1c73c16d2fdc8ee1eb8be
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-26 14:59:01 +0530

    Problems on Conditionals Statements

---
+++ Conditional Statements.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+  int x;
+  scanf("%d", &x);
+  if (x < 70) {
+    printf("Order Confirmed \n");
+    }  
+    else {
+    printf("Order Limit reached \n");
+    }
+    printf("Thank YOU!\n");
+   
+
+  scanf("%d", &x);
+  if (x < 70) {
+    printf("Order Confirmed \n");
+    }  
+    else {
+    printf("Order Limit reached \n");
+    }
+    printf("Thank YOU!");
+   
+  return 0;
+}
