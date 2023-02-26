c1889e60877ec63b4f1fb8791983a6ba1fa08728
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-26 15:44:52 +0530

    Area of any Rectangle

---
+++ Find the Area of any Rectangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int length, width;
+  int area ;
+  scanf("%d", &length);
+  scanf("%d", &width);
+  area = length * width;
+  printf("Area of the rectangle is: %d", area);
+  return 0;
+}
