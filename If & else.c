b8dd4543d9e714aa097441a4e45bb83d4ad8302e
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-26 14:29:15 +0530

    If Else in c

---
+++ If & else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int Age = 25 ;
+  int Vage = 18;
+
+  if (Age > Vage) {
+    printf("Old enough to vote!");
+  } else {
+    printf("Not old enough to vote.");
+  }
+  return 0;
+}
