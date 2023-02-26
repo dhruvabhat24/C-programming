45ace14ea9bc1eda134b54bec3d61277f28c52bf
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-26 15:41:25 +0530

    Calculator

---
+++ Calculator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int a,b;
+  int sum;
+  int diff;
+  scanf("%d %d", &a, &b);
+  sum = a + b ;
+  diff = a - b ;
+  printf("Sum is: %d \n", sum);
+  printf("Difference is: %d", diff );
+  return 0;
+}
